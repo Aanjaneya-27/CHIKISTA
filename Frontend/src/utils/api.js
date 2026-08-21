5d97d748de10b88e98ed1a5b79f08c0a162d7849
Aanjaneya Dikhit <dikhitaanjaneyat03@gmail.com>
2026-08-21 09:55:16 +0530

    add new api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://chikista.onrender.com/api",
+  baseURL: "https://chikista-1.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
