e07efca9eb56ab3986b5b7a20d9baa48556b5ad9
Aanjaneya Dikhit <dikhitaanjaneyat03@gmail.com>
2026-08-21 09:52:04 +0530

    new app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,52 +1,19 @@
-//  const express = require('express');
-//  const cors = require('cors');
-//  const userRoutes = require('./routes/userRoutes');
-//  const requisitionRoutes = require('./routes/requisitionRoutes');
-//  const masterRoutes = require('./routes/MasterRoutes');
-//  const rentalRoutes = require('./routes/rentalRoutes');
-
-// const app = express();
-
-// app.use(express.json());
-// app.use(cors());
-
-// app.use('/api/users', userRoutes);
-// app.use('/api/requisitions', requisitionRoutes);
-// app.use('/api/master', masterRoutes);
-// app.use("/api/rental", rentalRoutes);
-
-// module.exports = app;
-
-const express = require('express');
-const cors = require('cors');
-const userRoutes = require('./routes/userRoutes');
-const requisitionRoutes = require('./routes/requisitionRoutes');
-const masterRoutes = require('./routes/MasterRoutes');
-const rentalRoutes = require('./routes/rentalRoutes');
+ const express = require('express');
+ const cors = require('cors');
+ const userRoutes = require('./routes/userRoutes');
+ const requisitionRoutes = require('./routes/requisitionRoutes');
+ const masterRoutes = require('./routes/MasterRoutes');
+ const rentalRoutes = require('./routes/rentalRoutes');
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "https://chikista.vercel.app",
-      "http://localhost:5173",
-      "http://localhost:3000"
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
-  })
-);
-
-app.options("*", cors());
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api/users', userRoutes);
 app.use('/api/requisitions', requisitionRoutes);
 app.use('/api/master', masterRoutes);
-app.use('/api/rental', rentalRoutes);
+app.use("/api/rental", rentalRoutes);
 
 module.exports = app;
+
